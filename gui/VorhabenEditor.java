1f1a90b7fd9dc09a73655383adbe0c90d8532c16
mwaldau <marco.waldau@gmail.com>
2017-02-07 08:35:14 +0100

    07.02.2017 - 08:35

--- gui/VorhabenEditor.java
+++ gui/VorhabenEditor.java
@@ -6,11 +6,12 @@ import sun.management.snmp.jvmmib.JvmRTBootClassPathTableMeta;
 
 import javax.swing.*;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 /**
  * Created by rrose on 21.11.2016.
  */
-public class VorhabenEditor extends JPanel {
+public class VorhabenEditor extends VorhabenAnlegen {
 
     public static DateTimeFormatter FORMATTER;
     private JTextField name;
@@ -28,9 +29,12 @@ public class VorhabenEditor extends JPanel {
     private JButton ok;
     private JButton abbrechen;
 
-    public VorhabenEditor() {
 
-
+    public VorhabenEditor(List<Nutzer> soldaten, List<String> vorhabenListe) {
+        super(soldaten, vorhabenListe);
     }
 
+    public VorhabenEditor(List<Nutzer> soldaten, List<String> vorhabenListe, Vorhaben vorhaben) {
+        super(soldaten, vorhabenListe, vorhaben);
+    }
 }
