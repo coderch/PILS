c59170f60a96213c5ec4f4f273da246f96a9d4db
coderch <richard_rose@web.de>
2017-05-18 11:32:20 +0200

    18.05.17 rch

--- listener/PasswordResetListener.java
+++ listener/PasswordResetListener.java
@@ -7,15 +7,31 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- * Created by rrose on 18.05.2017.
+ * Dieser Listener ist für das Zurücksetzen der Passwörter zuständing. Ihm wird lediglich das JFormattedTextField übergeben, in welchem die
+ * Personalnummer des Nutzers wessen Passwort zurückgesetzt werden soll vorhanden ist.
+ * @see java.awt.event.ActionListener
+ * @author rrose
  */
 public class PasswordResetListener implements ActionListener {
+    /**
+     * Das Textfeld, in dem die Personalnummer des Nutzers vorhanden ist.
+     */
     private JFormattedTextField persNr;
 
+    /**
+     *
+     * @param persNr JFormattedTextField mit der Personalnummer.
+     */
     public PasswordResetListener(JFormattedTextField persNr) {
         this.persNr = persNr;
     }
 
+    /**
+     * Die Methode, welche bei Betätigung des zugeordneten Buttons ausgeführt wird. In dieser Methode wird mit Hilfe der Methode
+     * NutzerDAO.passwordZuruecksetzen(int personalnummer) das Passwort zurückgesetzt.
+     * @see NutzerDAO passwordZuruecksetzen().
+     * @param actionEvent
+     */
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
         if (!persNr.getText().equals("")) NutzerDAO.passwordZuruecksetzen(Integer.parseInt(persNr.getText()));
