6b4b2e26d3467d74ad0a3dc305391db3c1d7759c
coderch <richard_rose@web.de>
2017-05-29 14:03:23 +0200

    29.05.17 rch

--- db/VorhabenDAO.java
+++ db/VorhabenDAO.java
@@ -86,23 +86,23 @@ public class VorhabenDAO {
      */
     public static void vorhabenSpeichern(Vorhaben vorhaben, List<Nutzer> eingeteilteSoldaten) {
         try {
-            /**
-             * Speichert einen noch nicht vorhandenen Vorhaben-Namen in der Tabelle t_vorhaben. Überprüfung erfolgt über "ON CONFLICT DO".
-             * Ist dieser Name schon vorhanden führt dieses Statement keine weiteren Befehle aus.
+            /*
+              Speichert einen noch nicht vorhandenen Vorhaben-Namen in der Tabelle t_vorhaben. Überprüfung erfolgt über "ON CONFLICT DO".
+              Ist dieser Name schon vorhanden führt dieses Statement keine weiteren Befehle aus.
              */
             PreparedStatement pstm = DBConnect.preparedStatement("INSERT INTO t_vorhaben (pk_name) VALUES (?) ON CONFLICT DO NOTHING ");
             pstm.setString(1, vorhaben.getName());
             pstm.executeUpdate();
-            /**
-             * Speichert einen noch nicht vorhandenen Zeitraum in der Tabelle t_zeitraum. Überprüfung erfolgt über "ON CONFLICT DO".
-             * Ist dieser Zeitraum schon vorhanden führt dieses Statement keine weiteren Befehle aus.
+            /*
+              Speichert einen noch nicht vorhandenen Zeitraum in der Tabelle t_zeitraum. Überprüfung erfolgt über "ON CONFLICT DO".
+              Ist dieser Zeitraum schon vorhanden führt dieses Statement keine weiteren Befehle aus.
              */
             pstm = DBConnect.preparedStatement("INSERT INTO t_zeitraum (pk_von, pk_bis) VALUES (?,?) ON CONFLICT (pk_von,pk_bis) DO NOTHING");
             pstm.setDate(1, Date.valueOf(vorhaben.getStart()));
             pstm.setDate(2, Date.valueOf(vorhaben.getEnde()));
             pstm.executeUpdate();
-            /**
-             * Speichert das übergebene Vorhaben-Objekt in die Tabelle t_hat_vorhaben_im_zeitraum. Ist dieses Vorhaben bereits in dieser Tabelle vorhanden wird ein Update für die Vorhaben-Beschreibung durchgeführt.
+            /*
+              Speichert das übergebene Vorhaben-Objekt in die Tabelle t_hat_vorhaben_im_zeitraum. Ist dieses Vorhaben bereits in dieser Tabelle vorhanden wird ein Update für die Vorhaben-Beschreibung durchgeführt.
              */
             pstm = DBConnect.preparedStatement("INSERT INTO t_hat_vorhaben_im_zeitraum (fk_t_vorhaben_pk_t_name,beschreibung,fk_t_zeitraum_pk_von, fk_t_zeitraum_pk_bis, sonderdienst ) VALUES (?,?,?,?,?) ON CONFLICT (fk_t_vorhaben_pk_t_name,fk_t_zeitraum_pk_von,fk_t_zeitraum_pk_bis) DO UPDATE SET beschreibung = ?");
             pstm.setString(1, vorhaben.getName());
@@ -112,9 +112,9 @@ public class VorhabenDAO {
             pstm.setBoolean(5, vorhaben.getSonderdienst());
             pstm.setString(6, vorhaben.getBeschreibung());
             pstm.executeUpdate();
-            /**
-             * Speichert die einzelnen Nutzer aus der übergebenen Liste mit Zeitraum und zugeteiltem Vorhaben in die Tabelle t_nimmt_teil_am_vorhaben.
-             * Ist ein Nutzer bereits dem gleichen Vorhaben mit gleichem Zeitraum zugeordnet wird kein weiterer Datenbank-Befehl ausgeführt.
+            /*
+              Speichert die einzelnen Nutzer aus der übergebenen Liste mit Zeitraum und zugeteiltem Vorhaben in die Tabelle t_nimmt_teil_am_vorhaben.
+              Ist ein Nutzer bereits dem gleichen Vorhaben mit gleichem Zeitraum zugeordnet wird kein weiterer Datenbank-Befehl ausgeführt.
              */
             pstm = DBConnect.preparedStatement("INSERT INTO t_nimmt_teil_am_vorhaben (fk_t_soldat_pk_personalnummer, fk_t_vorhaben_pk_t_name, fk_t_zeitraum_pk_von, fk_t_zeitraum_pk_bis) VALUES (?,?,?,?) ON CONFLICT (fk_t_soldat_pk_personalnummer,fk_t_vorhaben_pk_t_name,fk_t_zeitraum_pk_von,fk_t_zeitraum_pk_bis) DO NOTHING ");
             for (Nutzer nutzer : eingeteilteSoldaten) {
@@ -138,8 +138,8 @@ public class VorhabenDAO {
     public static List<Nutzer> holeZugeteilteSoldaten(Vorhaben vorhaben) {
         List<Nutzer> eingeteilteSoldaten = new LinkedList<>();
         try {
-            /**
-             * SELECT Statement, welches durch einen INNER JOIN nur die dem Vorhaben zugeteilten Soldaten im passenden Zeitraum zurück gibt.
+            /*
+              SELECT Statement, welches durch einen INNER JOIN nur die dem Vorhaben zugeteilten Soldaten im passenden Zeitraum zurück gibt.
              */
             PreparedStatement pstm = DBConnect.preparedStatement("SELECT pk_personalnummer, dienstgrad, dienstgradgruppe,name, vorname, fk_t_rolle_pk_beschreibung  FROM t_nutzer INNER JOIN t_nimmt_teil_am_vorhaben ON pk_personalnummer = fk_t_soldat_pk_personalnummer WHERE fk_t_vorhaben_pk_t_name = ? AND fk_t_zeitraum_pk_von = ? AND fk_t_zeitraum_pk_bis = ?");
             pstm.setString(1, vorhaben.getName());
