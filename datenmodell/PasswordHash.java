fb95717b0115b6c8b37e0c417aa9fdd1e822cd9f
coderch <richard_rose@web.de>
2017-06-01 11:01:40 +0200

    01.06.2017

--- datenmodell/PasswordHash.java
+++ datenmodell/PasswordHash.java
@@ -19,14 +19,14 @@ public class PasswordHash {
     /**
      * Erzeugt mit Hilfe des SHA-256 Algorithmuses einen Hashwert des übergebenen Passwortes.
      *
-     * @param kennwort Das Kennwort für welches der SHA-Hash erzeugt werden soll.
+     * @param passwort Das Passwort für welches der SHA-Hash erzeugt werden soll.
      * @return Gibt einen String zurück mit dem SHA-256 Hashwert für das übergebene Passwort (64 Symbole von 0-f).
      */
-    public static String createHash(String kennwort) {
+    public static String createHash(String passwort) {
         StringBuilder sb = new StringBuilder();
         try {
             MessageDigest mDigest = MessageDigest.getInstance("SHA-256"); // Festlegen des Hash-Algorithmuses.
-            byte[] hashes = mDigest.digest(kennwort.getBytes()); // Zerlegen des Strings in ein Byte-Array und Berechnung der Hashes der einzelnen Bytes.
+            byte[] hashes = mDigest.digest(passwort.getBytes()); // Zerlegen des Strings in ein Byte-Array und Berechnung der Hashes der einzelnen Bytes.
             for (byte hash : hashes) {
                 sb.append(String.format("%02x",hash)); //Zusammenführung der errechneten Hashwerte zu einem zusammenhängenden String. Alternative Möglichkeit (Integer.toString((hash & 0xff) + 0x100, 16).substring(1)).
             }
